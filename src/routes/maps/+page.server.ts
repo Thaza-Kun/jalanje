68593cec1864e4f271ac49b16ec8754f3fd26e51
Thaza_Kun <61819672+Thaza-Kun@users.noreply.github.com>
2025-03-12 11:19:59 +0800

    :ambulance: HOTFIX: variable date, not constant

--- src/routes/maps/+page.server.ts
+++ src/routes/maps/+page.server.ts
@@ -1,75 +1,69 @@
 import {
-	GOOGLE_MAP_API_KEY,
-	TEST_DESTINATION_COORDINATE,
-	TEST_ORIGIN_COORDINATE,
+  GOOGLE_MAP_API_KEY,
+  TEST_DESTINATION_COORDINATE,
+  TEST_ORIGIN_COORDINATE,
 } from "$env/static/private";
 
 import { Coord as CoordInterface } from "./+page.svelte";
 
 class Coord implements CoordInterface {
-	location: { latLng: { latitude: number; longitude: number } };
+  location: { latLng: { latitude: number; longitude: number } };
 
-	constructor(latitude: number, longitude: number) {
-		this.location = {
-			latLng: {
-				latitude,
-				longitude,
-			},
-		};
-	}
+  constructor(latitude: number, longitude: number) {
+    this.location = {
+      latLng: {
+        latitude,
+        longitude,
+      },
+    };
+  }
 }
 
 export const load = async ({ fetch, cookies }) => {
-	const today: Date = new Date();
-	const times: Array<Date> = [...Array(24).keys()].map(
-		(hr) =>
-			new Date(
-				Date.UTC(
-					today.getUTCFullYear(),
-					today.getUTCMonth(),
-					// TODO: Find a way to get past timestamp
-					// Can only get future timestamp
-					today.getUTCDate() + 1,
-					hr,
-				),
-			),
-	);
-	const coord_origin: Coord = new Coord(
-		Number(TEST_ORIGIN_COORDINATE.split(",")[0]),
-		Number(TEST_ORIGIN_COORDINATE.split(",")[1]),
-	);
-	const coord_dest: Coord = new Coord(
-		Number(TEST_DESTINATION_COORDINATE.split(",")[0]),
-		Number(TEST_DESTINATION_COORDINATE.split(",")[1]),
-	);
+  const today: Date = new Date();
+  const times: Array<Date> = [...Array(24).keys()].map(
+    (hr) =>
+      new Date(
+        // TODO: Find a way to get past timestamp
+        // Can only get future timestamp
+        today.getTime() + hr * 60 * 60 * 1000 + 60,
+      ),
+  );
+  const coord_origin: Coord = new Coord(
+    Number(TEST_ORIGIN_COORDINATE.split(",")[0]),
+    Number(TEST_ORIGIN_COORDINATE.split(",")[1]),
+  );
+  const coord_dest: Coord = new Coord(
+    Number(TEST_DESTINATION_COORDINATE.split(",")[0]),
+    Number(TEST_DESTINATION_COORDINATE.split(",")[1]),
+  );
 
-	const requestRoute = (time: Date) =>
-		fetch("https://routes.googleapis.com/directions/v2:computeRoutes", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-				"X-Goog-Api-Key": GOOGLE_MAP_API_KEY,
-				"X-Goog-FieldMask":
-					"routes.duration,routes.staticDuration,routes.distanceMeters",
-				// TO INSPECT ALL FIELDS, USE *
-				// 'X-Goog-FieldMask': '*'
-			},
-			body: JSON.stringify({
-				origin: coord_origin,
-				destination: coord_dest,
-				travel_mode: "DRIVE",
-				routingPreference: "TRAFFIC_AWARE",
-				departure_time: time.toISOString(),
-			}),
-		});
-	const res = await Promise.all(
-		times.map(async (d) => await requestRoute(new Date())),
-	);
-	let item = await Promise.all(res.map(async (d) => (await d).json()));
-	return {
-		routes: item,
-		departures: times,
-		origin: JSON.parse(JSON.stringify(coord_origin)),
-		destination: JSON.parse(JSON.stringify(coord_dest)),
-	};
+  const requestRoute = (time: Date) =>
+    fetch("https://routes.googleapis.com/directions/v2:computeRoutes", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Goog-Api-Key": GOOGLE_MAP_API_KEY,
+        "X-Goog-FieldMask":
+          "routes.duration,routes.staticDuration,routes.distanceMeters",
+        // TO INSPECT ALL FIELDS, USE *
+        // 'X-Goog-FieldMask': '*'
+      },
+      body: JSON.stringify({
+        origin: coord_origin,
+        destination: coord_dest,
+        travel_mode: "DRIVE",
+        routingPreference: "TRAFFIC_AWARE",
+        departure_time: time.toISOString(),
+        regionCode: "my",
+      }),
+    });
+  const res = await Promise.all(times.map(async (d) => await requestRoute(d)));
+  let item = await Promise.all(res.map(async (d) => (await d).json()));
+  return {
+    routes: item,
+    departures: times,
+    origin: JSON.parse(JSON.stringify(coord_origin)),
+    destination: JSON.parse(JSON.stringify(coord_dest)),
+  };
 };
--- src/routes/maps/+page.svelte
+++ src/routes/maps/+page.svelte
@@ -1,66 +1,69 @@
-<svelte:head>
-  <title>Map</title>
-</svelte:head>
-
 <script lang="ts">
-interface Coord {
-	location: {
-		latLng: {
-			latitude: number;
-			longitude: number;
-		};
-	};
-}
-interface DistanceResponseProp {
-	distanceMeters: number;
-	duration: string; // in seconds => `{duration}s`
-	staticDuration: string; // in seconds => `{duration}s`
-}
-interface DistanceProp<T> {
-	distance: T; // in meters
-	duration: T; // in seconds
-	staticDuration: T; // in seconds
-}
-interface RouteResponseProp {
-	routes?: Array<DistanceResponseProp>;
-	error?: {
-		code: number;
-		message: string;
-		status: string;
-	};
-}
-interface Props<R> {
-	data: {
-		routes: Array<R>;
-		departures: Array<Date>;
-		origin: Coord;
-		destination: Coord;
-	};
-}
-import { onMount } from "svelte";
-let response : Props<RouteResponseProp> = $props();
+    interface Coord {
+        location: {
+            latLng: {
+                latitude: number;
+                longitude: number;
+            };
+        };
+    }
+    interface DistanceResponseProp {
+        distanceMeters: number;
+        duration: string; // in seconds => `{duration}s`
+        staticDuration: string; // in seconds => `{duration}s`
+    }
+    interface DistanceProp<T> {
+        distance: T; // in meters
+        duration: T; // in seconds
+        staticDuration: T; // in seconds
+    }
+    interface RouteResponseProp {
+        routes?: Array<DistanceResponseProp>;
+        error?: {
+            code: number;
+            message: string;
+            status: string;
+        };
+    }
+    interface Props<R> {
+        data: {
+            routes: Array<R>;
+            departures: Array<Date>;
+            origin: Coord;
+            destination: Coord;
+        };
+    }
+    import { onMount } from "svelte";
+    let response: Props<RouteResponseProp> = $props();
 
-let timeline: Props<DistanceProp<number | null>> = {
-	data: { ...response.data, routes: [] },
-};
-timeline.data.routes = response.data.routes.map((a) =>
-	a.routes
-		? {
-				distance: a.routes[0].distanceMeters,
-				duration: Number(a.routes[0].duration.replace("s", "")),
-				staticDuration: Number(a.routes[0].staticDuration.replace("s", "")),
-			}
-		: { distance: null, duration: null, staticDuration: null },
-);
+    let timeline: Props<DistanceProp<number | null>> = {
+        data: { ...response.data, routes: [] },
+    };
+    timeline.data.routes = response.data.routes.map((a) =>
+        a.routes
+            ? {
+                  distance: a.routes[0].distanceMeters,
+                  duration: Number(a.routes[0].duration.replace("s", "")),
+                  staticDuration: Number(
+                      a.routes[0].staticDuration.replace("s", ""),
+                  ),
+              }
+            : { distance: null, duration: null, staticDuration: null },
+    );
 
-onMount(() => {
-	console.log(timeline);
-});
+    onMount(() => {
+        console.log(response);
+        console.log(timeline);
+    });
 </script>
+
+<svelte:head>
+    <title>Map</title>
+</svelte:head>
 
 <!-- TODO: Chart -->
 <section>
-  {timeline.data.routes.map(
-			(r) => `${r.distance}m (${r.duration}s / ${r.staticDuration}s)`
-		)}
+    {timeline.data.routes.map(
+        (r) => `${r.distance}m (${r.duration}s / ${r.staticDuration}s)`,
+    )}
 </section>
